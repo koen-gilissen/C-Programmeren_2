b3bbd168c2d86a66266871f2e7bfab46f6961571
Koen Gilissen <koen.gilissen@pxl.be>
2018-02-01 15:16:18 +0100

    Arrays and pointers

---
+++ 2_CP2_s13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char* argv[])
+{
+	char mijnArray[] = "testArray";
+	char *mijnPointer = "testPointer";
+	printf( "%s -- %s\n", mijnArray, mijnPointer);
+    
+   return 0;
+}
